2c325648285e3eec5c59bc6c95c279fa4a785ed0
danagarzon2007 <grdaso08@gmail.com>
2024-10-25 15:04:53 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+?<php
+$Identificacion=$_POST ['Identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['cargo'];
+$Salario=$_POST ['salario'];
+echo "La identificacion del empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado son: ".NomApellidos;
+echo "El cargo del empleado es: ".Cargo;
+echo "El salario del empleado es:  ".Salario;
+?>
